3f09498966ba92a4973deec6ae2dde6f083c741e
Jiri Mencak <jmencak@users.noreply.github.com>
2021-12-13 21:31:47 +0100

    TuneD: switch to FDP TuneD delivery
    Switch to Fast Data Path (FDP) method of delivering TuneD.  Keep the old
    method of delivery in place for emergency rollback and update the
    version of TuneD to match the version current in the FDP channel.

--- assets/tuned/daemon/tuned/profiles/locator.py
+++ assets/tuned/daemon/tuned/profiles/locator.py
@@ -1,6 +1,12 @@
 import os
 import tuned.consts as consts
-from configobj import ConfigObj, ConfigObjError
+try:
+	from configparser import ConfigParser, Error
+	from io import StringIO
+except ImportError:
+	# python2.7 support, remove RHEL-7 support end
+	from ConfigParser import ConfigParser, Error
+	from StringIO import StringIO
 
 class Locator(object):
 	"""
@@ -25,6 +31,10 @@ class Locator(object):
 
 	def get_config(self, profile_name, skip_files=None):
 		ret = None
+		conditional_load = profile_name[0:1] == "-"
+		if conditional_load:
+			profile_name = profile_name[1:]
+
 		for dir_name in reversed(self._load_directories):
 			# basename is protection not to get out of the path
 			config_file = self._get_config_filename(dir_name, os.path.basename(profile_name))
@@ -35,6 +45,9 @@ class Locator(object):
 
 			if os.path.isfile(config_file):
 				return config_file
+
+		if conditional_load and ret is None:
+			ret = ""
 
 		return ret
 
@@ -48,8 +61,12 @@ class Locator(object):
 		if config_file is None:
 			return None
 		try:
-			return ConfigObj(config_file, list_values = False, interpolation = False)
-		except (IOError, OSError, ConfigObjError) as e:
+			config = ConfigParser()
+			config.optionxform = str
+			with open(config_file) as f:
+				config.readfp(StringIO("[" + consts.MAGIC_HEADER_NAME + "]\n" + f.read()))
+			return config
+		except (IOError, OSError, Error) as e:
 			return None
 
 	# Get profile attributes (e.g. summary, description), attrs is list of requested attributes,
@@ -75,17 +92,16 @@ class Locator(object):
 		config = self.parse_config(profile_name)
 		if config is None:
 			return [False, "", "", ""]
-		if consts.PLUGIN_MAIN_UNIT_NAME in config:
-			d = config[consts.PLUGIN_MAIN_UNIT_NAME]
-		else:
-			d = dict()
+		main_unit_in_config = consts.PLUGIN_MAIN_UNIT_NAME in config.sections()
 		vals = [True, profile_name]
 		for (attr, defval) in zip(attrs, defvals):
 			if attr == "" or attr is None:
 				vals[0] = False
 				vals = vals + [""]
+			elif main_unit_in_config and attr in config.options(consts.PLUGIN_MAIN_UNIT_NAME):
+				vals = vals + [config.get(consts.PLUGIN_MAIN_UNIT_NAME, attr, raw=True)]
 			else:
-				vals = vals + [d.get(attr, defval)]
+				vals = vals + [defval]
 		return vals
 
 	def list_profiles(self):
