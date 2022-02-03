e715a8f2fbcddecc5baeb54754a2895f52951ff4
Artyom Lukianov <alukiano@redhat.com>
2022-02-03 17:58:34 +0200

    Add performance-addon-operator as part of the nto manager
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- cmd/cluster-node-tuning-operator/main.go
+++ cmd/cluster-node-tuning-operator/main.go
@@ -6,14 +6,17 @@ import (
 	"path/filepath"
 	"runtime"
 
+	performancev2 "github.com/openshift-kni/performance-addon-operators/api/v2"
+	"github.com/openshift-kni/performance-addon-operators/controllers"
 	apiconfigv1 "github.com/openshift/api/config/v1"
 	mcov1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	apiruntime "k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
 	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 
 	tunedv1 "github.com/openshift/cluster-node-tuning-operator/pkg/apis/tuned/v1"
 	"github.com/openshift/cluster-node-tuning-operator/pkg/config"
@@ -23,14 +26,15 @@ import (
 	"github.com/openshift/cluster-node-tuning-operator/pkg/tuned"
 	"github.com/openshift/cluster-node-tuning-operator/pkg/util"
 	"github.com/openshift/cluster-node-tuning-operator/version"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
 )
 
 const (
 	operandFilename  = "openshift-tuned"
 	operatorFilename = "cluster-node-tuning-operator"
-	metricsHost      = "0.0.0.0"
+	webhookPort      = 4343
+	webhookCertDir   = "/apiserver.local.config/certificates"
+	webhookCertName  = "apiserver.crt"
+	webhookKeyName   = "apiserver.key"
 )
 
 var (
@@ -42,6 +46,7 @@ func init() {
 	utilruntime.Must(tunedv1.AddToScheme(scheme))
 	utilruntime.Must(mcov1.AddToScheme(scheme))
 	utilruntime.Must(apiconfigv1.Install(scheme))
+	utilruntime.Must(performancev2.AddToScheme(scheme))
 }
 
 func printVersion() {
@@ -100,11 +105,36 @@ func main() {
 
 		controller, err := operator.NewController()
 		if err != nil {
-			klog.Fatal(err)
+			klog.Fatalf("failed to create new controller: %v", err)
 		}
-		mgr.Add(controller)
-		mgr.Add(metrics.Server{})
+
+		if err := mgr.Add(controller); err != nil {
+			klog.Fatalf("failed to add new controller to the manager: %v", err)
+		}
+
+		if err := mgr.Add(metrics.Server{}); err != nil {
+			klog.Fatalf("unable to add metrics server as runnable under the manager: %v", err)
+		}
 		metrics.RegisterVersion(version.Version)
+
+		if err = (&controllers.PerformanceProfileReconciler{
+			Client:   mgr.GetClient(),
+			Scheme:   mgr.GetScheme(),
+			Recorder: mgr.GetEventRecorderFor("performance-profile-controller"),
+		}).SetupWithManager(mgr); err != nil {
+			klog.Exitf("unable to create PerformanceProfile controller: %v", err)
+		}
+
+		// configure webhook server
+		webHookServer := mgr.GetWebhookServer()
+		webHookServer.Port = webhookPort
+		webHookServer.CertDir = webhookCertDir
+		webHookServer.CertName = webhookCertName
+		webHookServer.KeyName = webhookKeyName
+
+		if err = (&performancev2.PerformanceProfile{}).SetupWebhookWithManager(mgr); err != nil {
+			klog.Exitf("unable to create PerformanceProfile v2 webhook: %v", err)
+		}
 
 		if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 			klog.Exitf("manager exited with non-zero code: %v", err)
