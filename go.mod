f90622919c2df852a9f0ac218a3ff79be404d6a9
Yanir Quinn <yquinn@redhat.com>
2022-02-08 11:28:48 +0200

    wrap NTO controller with controller runtime lib
    This is a refactor to the main invocation of
    cluster node tuning controller.
    The controller and the metrics server are wrapped and
    started by the controller runtime library.

--- go.mod
+++ go.mod
@@ -4,10 +4,9 @@ go 1.17
 
 require (
 	github.com/coreos/ignition/v2 v2.7.0
-	github.com/google/uuid v1.1.2
 	github.com/kevinburke/go-bindata v3.16.0+incompatible
-	github.com/onsi/ginkgo v1.14.0
-	github.com/onsi/gomega v1.10.1
+	github.com/onsi/ginkgo v1.16.5
+	github.com/onsi/gomega v1.17.0
 	github.com/openshift/api v0.0.0-20211209135129-c58d9f695577
 	github.com/openshift/build-machinery-go v0.0.0-20211213093930-7e33a7eb4ce3
 	github.com/openshift/client-go v0.0.0-20211209144617-7385dd6338e3
@@ -24,6 +23,7 @@ require (
 	k8s.io/klog v1.0.0
 	k8s.io/klog/v2 v2.30.0
 	k8s.io/utils v0.0.0-20210930125809-cb0fa318a74b
+	sigs.k8s.io/controller-runtime v0.11.0
 	sigs.k8s.io/controller-tools v0.4.0
 )
 
@@ -39,31 +39,32 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful v2.10.0+incompatible // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
-	github.com/fatih/color v1.7.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-logr/logr v1.2.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
-	github.com/gobuffalo/flect v0.2.0 // indirect
+	github.com/gobuffalo/flect v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
-	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/nxadm/tail v1.4.4 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.28.0 // indirect
 	github.com/prometheus/procfs v0.6.0 // indirect
@@ -73,12 +74,13 @@ require (
 	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
-	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
+	golang.org/x/sys v0.0.0-20211029165221-6e7872819dc8 // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 	golang.org/x/tools v0.1.6-0.20210820212750-d4cc65f0b2ff // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
@@ -86,11 +88,12 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/apiextensions-apiserver v0.23.0 // indirect
+	k8s.io/component-base v0.23.0 // indirect
 	k8s.io/gengo v0.0.0-20210813121822-485abfe95c7c // indirect
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
 	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.0 // indirect
-	sigs.k8s.io/yaml v1.2.0 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
 // Pinned to kubernetes-1.23.0
@@ -105,6 +108,8 @@ replace (
 	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.23.0
 	k8s.io/code-generator => k8s.io/code-generator v0.23.0
 	k8s.io/component-base => k8s.io/component-base v0.23.0
+	k8s.io/component-helpers => k8s.io/component-helpers v0.23.0
+	k8s.io/controller-manager => k8s.io/controller-manager v0.23.0
 	k8s.io/cri-api => k8s.io/cri-api v0.23.0
 	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.23.0
 	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.23.0
@@ -116,5 +121,8 @@ replace (
 	k8s.io/kubernetes => k8s.io/kubernetes v1.23.0
 	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.23.0
 	k8s.io/metrics => k8s.io/metrics v0.23.0
+	k8s.io/mount-utils => k8s.io/mount-utils v0.23.0
 	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.23.0
+	sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.11.0
+	sigs.k8s.io/controller-tools => sigs.k8s.io/controller-tools v0.7.0
 )
--- vendor/sigs.k8s.io/controller-tools/pkg/crd/gen.go
+++ vendor/sigs.k8s.io/controller-tools/pkg/crd/gen.go
@@ -18,10 +18,10 @@ package crd
 
 import (
 	"fmt"
+	"go/ast"
 	"go/types"
 
 	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	apiextlegacy "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	crdmarkers "sigs.k8s.io/controller-tools/pkg/crd/markers"
@@ -31,31 +31,16 @@ import (
 	"sigs.k8s.io/controller-tools/pkg/version"
 )
 
+// The identifier for v1 CustomResourceDefinitions.
+const v1 = "v1"
+
 // The default CustomResourceDefinition version to generate.
-const defaultVersion = "v1"
+const defaultVersion = v1
 
 // +controllertools:marker:generateHelp
 
 // Generator generates CustomResourceDefinition objects.
 type Generator struct {
-	// TrivialVersions indicates that we should produce a single-version CRD.
-	//
-	// Single "trivial-version" CRDs are compatible with older (pre 1.13)
-	// Kubernetes API servers.  The storage version's schema will be used as
-	// the CRD's schema.
-	//
-	// Only works with the v1beta1 CRD version.
-	TrivialVersions bool `marker:",optional"`
-
-	// PreserveUnknownFields indicates whether or not we should turn off pruning.
-	//
-	// Left unspecified, it'll default to true when only a v1beta1 CRD is
-	// generated (to preserve compatibility with older versions of this tool),
-	// or false otherwise.
-	//
-	// It's required to be false for v1 CRDs.
-	PreserveUnknownFields *bool `marker:",optional"`
-
 	// AllowDangerousTypes allows types which are usually omitted from CRD generation
 	// because they are not recommended.
 	//
@@ -76,14 +61,22 @@ type Generator struct {
 	// CRDVersions specifies the target API versions of the CRD type itself to
 	// generate. Defaults to v1.
 	//
+	// Currently, the only supported value is v1.
+	//
 	// The first version listed will be assumed to be the "default" version and
 	// will not get a version suffix in the output filename.
 	//
 	// You'll need to use "v1" to get support for features like defaulting,
 	// along with an API server that supports it (Kubernetes 1.16+).
 	CRDVersions []string `marker:"crdVersions,optional"`
+
+	// GenerateEmbeddedObjectMeta specifies if any embedded ObjectMeta in the CRD should be generated
+	GenerateEmbeddedObjectMeta *bool `marker:",optional"`
 }
 
+func (Generator) CheckFilter() loader.NodeFilter {
+	return filterTypesForCRDs
+}
 func (Generator) RegisterMarkers(into *markers.Registry) error {
 	return crdmarkers.Register(into)
 }
@@ -93,6 +86,8 @@ func (g Generator) Generate(ctx *genall.GenerationContext) error {
 		Checker:   ctx.Checker,
 		// Perform defaulting here to avoid ambiguity later
 		AllowDangerousTypes: g.AllowDangerousTypes != nil && *g.AllowDangerousTypes == true,
+		// Indicates the parser on whether to register the ObjectMeta type or not
+		GenerateEmbeddedObjectMeta: g.GenerateEmbeddedObjectMeta != nil && *g.GenerateEmbeddedObjectMeta == true,
 	}
 
 	AddKnownTypes(parser)
@@ -124,6 +119,9 @@ func (g Generator) Generate(ctx *genall.GenerationContext) error {
 		crdRaw := parser.CustomResourceDefinitions[groupKind]
 		addAttribution(&crdRaw)
 
+		// Prevent the top level metadata for the CRD to be generate regardless of the intention in the arguments
+		FixTopLevelMetadata(crdRaw)
+
 		versionedCRDs := make([]interface{}, len(crdVersions))
 		for i, ver := range crdVersions {
 			conv, err := AsVersion(crdRaw, schema.GroupVersion{Group: apiext.SchemeGroupVersion.Group, Version: ver})
@@ -133,29 +131,8 @@ func (g Generator) Generate(ctx *genall.GenerationContext) error {
 			versionedCRDs[i] = conv
 		}
 
-		if g.TrivialVersions {
-			for i, crd := range versionedCRDs {
-				if crdVersions[i] == "v1beta1" {
-					toTrivialVersions(crd.(*apiextlegacy.CustomResourceDefinition))
-				}
-			}
-		}
-
-		// *If* we're only generating v1beta1 CRDs, default to `preserveUnknownFields: (unset)`
-		// for compatibility purposes.  In any other case, default to false, since that's
-		// the sensible default and is required for v1.
-		v1beta1Only := len(crdVersions) == 1 && crdVersions[0] == "v1beta1"
-		switch {
-		case (g.PreserveUnknownFields == nil || *g.PreserveUnknownFields) && v1beta1Only:
-			crd := versionedCRDs[0].(*apiextlegacy.CustomResourceDefinition)
-			crd.Spec.PreserveUnknownFields = nil
-		case g.PreserveUnknownFields == nil, g.PreserveUnknownFields != nil && !*g.PreserveUnknownFields:
-			// it'll be false here (coming from v1) -- leave it as such
-		default:
-			return fmt.Errorf("you may only set PreserveUnknownFields to true with v1beta1 CRDs")
-		}
-
 		for i, crd := range versionedCRDs {
+			removeDescriptionFromMetadata(crd.(*apiext.CustomResourceDefinition))
 			var fileName string
 			if i == 0 {
 				fileName = fmt.Sprintf("%s_%s.yaml", crdRaw.Spec.Group, crdRaw.Spec.Names.Plural)
@@ -171,30 +148,35 @@ func (g Generator) Generate(ctx *genall.GenerationContext) error {
 	return nil
 }
 
-// toTrivialVersions strips out all schemata except for the storage schema,
-// and moves that up into the root object.  This makes the CRD compatible
-// with pre 1.13 clusters.
-func toTrivialVersions(crd *apiextlegacy.CustomResourceDefinition) {
-	var canonicalSchema *apiextlegacy.CustomResourceValidation
-	var canonicalSubresources *apiextlegacy.CustomResourceSubresources
-	var canonicalColumns []apiextlegacy.CustomResourceColumnDefinition
-	for i, ver := range crd.Spec.Versions {
-		if ver.Storage == true {
-			canonicalSchema = ver.Schema
-			canonicalSubresources = ver.Subresources
-			canonicalColumns = ver.AdditionalPrinterColumns
+func removeDescriptionFromMetadata(crd *apiext.CustomResourceDefinition) {
+	for _, versionSpec := range crd.Spec.Versions {
+		if versionSpec.Schema != nil {
+			removeDescriptionFromMetadataProps(versionSpec.Schema.OpenAPIV3Schema)
 		}
-		crd.Spec.Versions[i].Schema = nil
-		crd.Spec.Versions[i].Subresources = nil
-		crd.Spec.Versions[i].AdditionalPrinterColumns = nil
 	}
-	if canonicalSchema == nil {
-		return
-	}
+}
 
-	crd.Spec.Validation = canonicalSchema
-	crd.Spec.Subresources = canonicalSubresources
-	crd.Spec.AdditionalPrinterColumns = canonicalColumns
+func removeDescriptionFromMetadataProps(v *apiext.JSONSchemaProps) {
+	if m, ok := v.Properties["metadata"]; ok {
+		meta := &m
+		if meta.Description != "" {
+			meta.Description = ""
+			v.Properties["metadata"] = m
+
+		}
+	}
+}
+
+// FixTopLevelMetadata resets the schema for the top-level metadata field which is needed for CRD validation
+func FixTopLevelMetadata(crd apiext.CustomResourceDefinition) {
+	for _, v := range crd.Spec.Versions {
+		if v.Schema != nil && v.Schema.OpenAPIV3Schema != nil && v.Schema.OpenAPIV3Schema.Properties != nil {
+			schemaProperties := v.Schema.OpenAPIV3Schema.Properties
+			if _, ok := schemaProperties["metadata"]; ok {
+				schemaProperties["metadata"] = apiext.JSONSchemaProps{Type: "object"}
+			}
+		}
+	}
 }
 
 // addAttribution adds attribution info to indicate controller-gen tool was used
@@ -276,4 +258,23 @@ func FindKubeKinds(parser *Parser, metav1Pkg *loader.Package) map[schema.GroupKi
 	}
 
 	return kubeKinds
+}
+
+// filterTypesForCRDs filters out all nodes that aren't used in CRD generation,
+// like interfaces and struct fields without JSON tag.
+func filterTypesForCRDs(node ast.Node) bool {
+	switch node := node.(type) {
+	case *ast.InterfaceType:
+		// skip interfaces, we never care about references in them
+		return false
+	case *ast.StructType:
+		return true
+	case *ast.Field:
+		_, hasTag := loader.ParseAstTag(node.Tag).Lookup("json")
+		// fields without JSON tags mean we have custom serialization,
+		// so only visit fields with tags.
+		return hasTag
+	default:
+		return true
+	}
 }
