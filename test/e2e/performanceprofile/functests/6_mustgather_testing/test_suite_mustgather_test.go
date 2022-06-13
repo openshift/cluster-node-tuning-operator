84bbf61d411a335352d72b0a737c7579c5ba7ac6
Mario Fernandez <mariofer@redhat.com>
2022-06-13 13:04:55 +0200

    must-gather e2e test
    Add e2e must-gather test with pre-collected data.
    Untar sysinfo.tgz
    Check if generated files are present
    
    Signed-off-by: Mario Fernandez <mariofer@redhat.com>

---
+++ test/e2e/performanceprofile/functests/6_mustgather_testing/test_suite_mustgather_test.go
@@ -0,0 +1,55 @@
+package pao_mustgather
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+
+	ginkgo_reporters "kubevirt.io/qe-tools/pkg/ginkgo-reporters"
+
+	testutils "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils"
+	testclient "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/client"
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/junit"
+	testlog "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/log"
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/namespaces"
+)
+
+var _ = BeforeSuite(func() {
+	Expect(testclient.ClientsEnabled).To(BeTrue(), "package client not enabled")
+	// create test namespace
+	err := testclient.Client.Create(context.TODO(), namespaces.TestingNamespace)
+	if errors.IsAlreadyExists(err) {
+		testlog.Warning("test namespace already exists, that is unexpected")
+		return
+	}
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	err := testclient.Client.Delete(context.TODO(), namespaces.TestingNamespace)
+	Expect(err).ToNot(HaveOccurred())
+	err = namespaces.WaitForDeletion(testutils.NamespaceTesting, 5*time.Minute)
+	Expect(err).ToNot(HaveOccurred())
+	snapshotProc := filepath.Join(snapshotDir, "proc")
+	snapshotSys := filepath.Join(snapshotDir, "sys")
+	os.RemoveAll(snapshotProc)
+	os.RemoveAll(snapshotSys)
+})
+
+func TestPaoMustgatherTests(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	rr := []Reporter{}
+	if ginkgo_reporters.Polarion.Run {
+		rr = append(rr, &ginkgo_reporters.Polarion)
+	}
+	rr = append(rr, junit.NewJUnitReporter("must-gather"))
+	RunSpecsWithDefaultAndCustomReporters(t, "PAO must-gather tests", rr)
+}
