d9546008a8be899affc6f319e64b930a0b9d8069
Mario Fernandez <mariofer@redhat.com>
2022-08-30 11:51:32 +0200

    Add test ethtool must-gather
    Signed-off-by: Mario Fernandez <mariofer@redhat.com>

--- test/e2e/performanceprofile/functests/6_mustgather_testing/mustgather.go
+++ test/e2e/performanceprofile/functests/6_mustgather_testing/mustgather.go
@@ -131,6 +131,8 @@ var _ = Describe("[rfe_id: 50649] Performance Addon Operator Must Gather", func(
 				"podresources.json",
 				"proc_cmdline",
 				"sysinfo.log",
+				"ethtool_features",
+				"ethtool_channels",
 			}
 
 			err = checkfilesExist(nodeSpecificFiles, snapShotPath)
