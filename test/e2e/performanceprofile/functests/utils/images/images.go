7b5eec0ba90cfe656cc50ee39a78e5614ea465b0
Francesco Romani <fromani@redhat.com>
2023-03-15 08:09:34 +0100

    tests: image: bump to the latest version
    We should try to avoid floating tag (`latest`). Release-referring
    floating tags are the lesser evil, so it's OK(-ish).
    
    Ideally, the tests should detect the cluster version and pick the tests
    version automatically by default.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/performanceprofile/functests/utils/images/images.go
+++ test/e2e/performanceprofile/functests/utils/images/images.go
@@ -13,7 +13,7 @@ func init() {
 	cnfTestsImage = os.Getenv("CNF_TESTS_IMAGE")
 
 	if cnfTestsImage == "" {
-		cnfTestsImage = "cnf-tests:4.9"
+		cnfTestsImage = "cnf-tests:4.14"
 	}
 
 	if registry == "" {
