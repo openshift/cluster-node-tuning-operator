6921fbb9d4bbfd4ed66460767730ac4511acb622
Jiri Mencak <jmencak@redhat.com>
2023-04-03 16:53:41 +0200

    Update to the latest k8s and OpenShift deps (#580)
    Update:
      - to k8s 1.26.2
      - to latest OpenShift dependencies
      - to latest MCO dependencies (unpinned)
    
    Other changes:
      - Makefile:
        - reflect build-machinery-go changes
        - pin on controller-gen v0.6.0
      - kubeletconfig_test.go fix to reflect the latest k8s changes
      - make update-manifests
      - go mod tidy && go mod vendor
    
    Co-authored-by: Jiri Mencak <jmencak@users.noreply.github.com>

--- pkg/generated/clientset/versioned/clientset.go
+++ pkg/generated/clientset/versioned/clientset.go
@@ -17,8 +17,7 @@ type Interface interface {
 	TunedV1() tunedv1.TunedV1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	tunedV1 *tunedv1.TunedV1Client
