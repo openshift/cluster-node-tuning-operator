94c21e13764e8a013549ca126f3e42d3b863114b
Niranjan M.R <niranjan@ashoo.in>
2023-05-31 10:04:52 +0530

    OCPBUGS-5529: Fix updating numa core siblings map in GetCpuSiblings function (#564)
    * Fix updating numa core siblings map in GetCpuSiblings function
    
    move the delete operation outside of for loop.
    Also minor fixes to offline tests to check if the worker node
    has 20 cores earlier in the test function
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * check if the numacoresiblings map cotains appropriate cpu core key
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * Rename GetCpuSiblings function to GetAndRemoveCpuSiblingsFromMap
    
    Update all occurances fo getCpuSiblings to GetAndRemoveCpuSiblingsFromMap
    in the test cases
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * Rename GetCpuSiblings to GetAndRemoveCpuSiblingsFromMap
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    ---------
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    Co-authored-by: Niranjan M.R <mrniranjan@redhat.com>

--- test/e2e/performanceprofile/functests/utils/nodes/nodes.go
+++ test/e2e/performanceprofile/functests/utils/nodes/nodes.go
@@ -441,14 +441,21 @@ func GetByCpuCapacity(nodesList []corev1.Node, cpuQty int) []corev1.Node {
 	return nodesWithSufficientCpu
 }
 
-// GetCpuSiblings function returns the cpus siblings associated with core
+// GetAndRemoveCpuSiblingsFromMap function returns the cpus siblings associated with core
 // Also updates the map by deleting the cpu siblings returned
-func GetCpuSiblings(numaCoreSiblings map[int]map[int][]int, coreKey int) []string {
+func GetAndRemoveCpuSiblingsFromMap(numaCoreSiblings map[int]map[int][]int, coreId int) []string {
 	var cpuSiblings []string
-	for key := range numaCoreSiblings {
-		for _, c := range numaCoreSiblings[key][coreKey] {
-			cpuSiblings = append(cpuSiblings, strconv.Itoa(c))
-			delete(numaCoreSiblings[key], coreKey)
+	// Iterate over the  Numa node in the map
+	for node := range numaCoreSiblings {
+		// Check if the coreId exists in the Numa node
+		_, ok := numaCoreSiblings[node][coreId]
+		if ok {
+			// Iterate over the siblings of the coreId
+			for _, sibling := range numaCoreSiblings[node][coreId] {
+				cpuSiblings = append(cpuSiblings, strconv.Itoa(sibling))
+			}
+			// Delete the cpusiblings of that particular coreid
+			delete(numaCoreSiblings[node], coreId)
 		}
 	}
 	return cpuSiblings
