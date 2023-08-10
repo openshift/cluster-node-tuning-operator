de58e24ee3a2e65ece5a2ed14888e7aaac20fcb6
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-08-10 14:13:30 +0000

    nto:tuned: remove sched_min_granularity_ns settings (#726)
    sched_min_granularity_ns was dropped from Red Hat-maintained profiles.
    we should align with the other profiles and drop the value from NTO as well.
    
    In addition, we drop the test that checks for the value
    and update the rendered profiles.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    Co-authored-by: Talor Itzhak <titzhak@redhat.com>

--- test/e2e/performanceprofile/functests/1_performance/performance.go
+++ test/e2e/performanceprofile/functests/1_performance/performance.go
@@ -404,8 +404,7 @@ var _ = Describe("[rfe_id:27368][performance]", Ordered, func() {
 				"vm.swappiness":             "10",
 			}
 			schedulerKnobs := map[string]string{
-				"min_granularity_ns": "10000000",
-				"migration_cost_ns":  "5000000",
+				"migration_cost_ns": "5000000",
 			}
 
 			key := types.NamespacedName{
