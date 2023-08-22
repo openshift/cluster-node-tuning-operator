94c2c7519f7ec82b82669dddf66716bf8a590431
Forrest Babcock <fbabcock@redhat.com>
2023-08-22 09:31:21 -0400

    Revert "Tighten the rules for modifying Tuned Profiles (#765)"
    This reverts commit 561d9ae80b34c9196707d20d4752fb93474f1b0a.

--- pkg/operator/profilecalculator.go
+++ pkg/operator/profilecalculator.go
@@ -24,6 +24,9 @@ const (
 )
 
 type tunedState struct {
+	profileLastUpdatedGeneration map[string]int64
+	// Node name:                    ^^^^^^
+	// Last time operator changed profile:  ^^^
 	nodeLabels map[string]map[string]string
 	// Node name:  ^^^^^^
 	// Node-specific label:   ^^^^^^
@@ -34,9 +37,6 @@ type tunedState struct {
 	providerIDs map[string]string
 	// Node name:   ^^^^^^
 	// provider-id         ^^^^^^
-	bootcmdline map[string]string
-	// Node name:   ^^^^^^
-	// bootcmdline         ^^^^^^
 }
 
 type ProfileCalculator struct {
@@ -53,7 +53,7 @@ func NewProfileCalculator(listers *ntoclient.Listers, clients *ntoclient.Clients
 	pc.state.nodeLabels = map[string]map[string]string{}
 	pc.state.podLabels = map[string]map[string]map[string]string{}
 	pc.state.providerIDs = map[string]string{}
-	pc.state.bootcmdline = map[string]string{}
+	pc.state.profileLastUpdatedGeneration = map[string]int64{}
 	return pc
 }
 
@@ -130,10 +130,6 @@ func (pc *ProfileCalculator) nodeChangeHandler(nodeName string) (bool, error) {
 		pc.state.providerIDs[nodeName] = node.Spec.ProviderID
 		klog.V(3).Infof("Node's %s providerID=%v", nodeName, node.Spec.ProviderID)
 		change = true
-	}
-
-	if node.ObjectMeta.Annotations != nil {
-		pc.state.bootcmdline[nodeName] = node.ObjectMeta.Annotations[tunedv1.TunedBootcmdlineAnnotationKey]
 	}
 
 	nodeLabelsNew := util.MapOfStringsCopy(node.Labels)
