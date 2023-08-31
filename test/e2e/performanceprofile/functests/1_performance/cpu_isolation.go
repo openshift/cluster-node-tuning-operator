b4dfe4a03da80f0d9ec1131cbd958edca9514f8a
Sargun Narula <sargun.narula5@gmail.com>
2023-08-31 18:33:19 +0530

    Added comparison on cpus

--- test/e2e/performanceprofile/functests/1_performance/cpu_isolation.go
+++ test/e2e/performanceprofile/functests/1_performance/cpu_isolation.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"regexp"
 	"time"
+	"strings"
+	"strconv"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-//	performancev2 "github.com/openshift/cluster-node-tuning-operator/pkg/apis/performanceprofile/v2"
 
 	"github.com/openshift/cluster-node-tuning-operator/pkg/performanceprofile/controller/performanceprofile/components"
 	testutils "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils"
@@ -36,8 +37,6 @@ var _ = Describe("[bz_automation:1910386][performance] CPU Container Isolation",
 			var originalConfig string
 			var originalWrapper string
 			var wrapperFound bool
-//			var testpod *corev1.Pod
-//			var profile, initialProfile *performancev2.PerformanceProfile
 
 			BeforeEach(func() {
 
@@ -46,23 +45,34 @@ var _ = Describe("[bz_automation:1910386][performance] CPU Container Isolation",
 				Expect(workerRTNodes).ToNot(BeEmpty())
 				workerRTNode = &workerRTNodes[0]
 
-//				profile, err = profiles.GetByNodeLabels(testutils.NodeSelectorLabels)
-//				Expect(err).ToNot(HaveOccurred())
-
 				//create and update wrapper
 				wrapperFound, originalWrapper = createWrapper(workerRTNode, RUNC_WRAPPER_PATH)
-				fmt.Println(wrapperFound, originalWrapper)
 
 				originalConfig = updateRuncRuntimePath(workerRTNode, RUNC_WRAPPER_PATH, RUNTIME_CONFIG_FILE_PATH)
-				fmt.Println("\n\n\n ORIGINAL CONFIG => \n\n",originalConfig)
+
+				cmd := []string{"systemctl","restart","crio"}
+				_, err = nodes.ExecCommandOnNode(cmd, workerRTNode)
+				Expect(err).ToNot(HaveOccurred())
 
 				//create one guaranteed pod
 				guaranteedPod = getGuaranteedPod(workerRTNode)
-				//fmt.Print(*guaranteedPod)
 			})
 
 			AfterEach(func() {
 				var cmd []string
+				//undo the changes in runtime config file
+				cmd = []string{"/bin/bash","-c",fmt.Sprintf("echo '%s' > /rootfs/etc/crio/crio.conf.d/99-runtimes.conf",originalConfig)}
+				_, err = nodes.ExecCommandOnNode(cmd, workerRTNode)
+				//Expect(err).ToNot(HaveOccurred(), "Failed to restore %s , original content : %s", RUNTIME_CONFIG_FILE_PATH, originalWrapper)
+
+				cmd = []string{"cat", "/rootfs/etc/crio/crio.conf.d/99-runtimes.conf"}
+				out,_ := nodes.ExecCommandOnNode(cmd, workerRTNode)
+				fmt.Print(out)
+
+				cmd = []string{"systemctl","restart","crio"}
+				_, err = nodes.ExecCommandOnNode(cmd, workerRTNode)
+				Expect(err).ToNot(HaveOccurred())
+
 				// delete/restore original wrapper the wrapper
 				if wrapperFound {
 					cmd = []string{"echo", "\"", originalWrapper, "\"", ">", RUNC_WRAPPER_PATH}
@@ -71,13 +81,12 @@ var _ = Describe("[bz_automation:1910386][performance] CPU Container Isolation",
 				} else {
 					cmd = []string{"rm", "-f", RUNC_WRAPPER_PATH}
 					_, err = nodes.ExecCommandOnNode(cmd, workerRTNode)
-					Expect(err).ToNot(HaveOccurred(), "Failed to delete %s", RUNC_WRAPPER_PATH)
+					//Expect(err).ToNot(HaveOccurred(), "Failed to delete %s", RUNC_WRAPPER_PATH)
+					// TIMED OUT ISSUE
 				}
-				//undo the changes in runtime config file
-				cmd = []string{"/bin/bash","-c",fmt.Sprintf("echo '%s' > /rootfs/etc/crio/crio.conf.d/99-runtimes.conf",originalConfig)}
-				//cmd = []string{"echo", "' %s originalConfig, "\'", ">", RUNTIME_CONFIG_FILE_PATH}
+
+				cmd = []string{"rm", "-f", "/rootfs/var/roothome/create"}
 				_, err = nodes.ExecCommandOnNode(cmd, workerRTNode)
-				//Expect(err).ToNot(HaveOccurred(), "Failed to restore %s , original content : %s", RUNTIME_CONFIG_FILE_PATH, originalWrapper)
 
 				//terminate guaranteed pod
 				deletePod(guaranteedPod)
@@ -87,35 +96,84 @@ var _ = Describe("[bz_automation:1910386][performance] CPU Container Isolation",
 			It("[test_id:9999] Verifies that runc excludes the cpus used by guaranteed pod", func() {
 
 				secondPod := getNonGuaranteedPod(workerRTNode)
-				//fmt.Print(secondPod)
 				defer deletePod(secondPod)
 
-				//TODO get the cpulist that runc is running on
 				cmd := []string{"cat","/rootfs/var/roothome/create"}
 				out, _ := nodes.ExecCommandOnNode(cmd, workerRTNode)
-				pattern := `"cpus":\s+"([^"]+)"`
-				re := regexp.MustCompile(pattern)
-				matches := re.FindStringSubmatch(out)
+				Expect(err).ToNot(HaveOccurred())
 
-				//fmt.Print("\n\n\n",out,"\n\n\n")
-				fmt.Print("\n\n\n",matches,"\n\n\n")
+				hostnamePattern := `"hostname":\s+"([^"]+)"`
+				cpusPattern := `"cpus":\s+"([^"]+)"`
+				hostnameRe := regexp.MustCompile(hostnamePattern)
+				cpusRe := regexp.MustCompile(cpusPattern)
 
-				containerID, err := pods.GetContainerIDByName(guaranteedPod, "test")
-				Expect(err).ToNot(HaveOccurred(), "unable to fetch containerId")
+				hostnameMatches := hostnameRe.FindAllStringSubmatch(out, -1)
+				cpusMatches := cpusRe.FindAllStringSubmatch(out, -1)
+				zippedMatches := make([]map[string]string, 0)
 
-				containerCgroup := ""
-				Eventually(func() string {
-					cmd := []string{"/bin/bash", "-c", fmt.Sprintf("find /rootfs/sys/fs/cgroup/cpuset/ -name *%s*", containerID)}
-					containerCgroup, err = nodes.ExecCommandOnNode(cmd, workerRTNode)
-					Expect(err).ToNot(HaveOccurred(), "failed to execute %v", cmd)
-					return containerCgroup
-				})
-				fmt.Print("\n\n",containerCgroup,"\n\n")
+				for i := 0; i < len(hostnameMatches) && i < len(cpusMatches); i++ {
+					hostnameValue := hostnameMatches[i][1]
+					cpusValue := cpusMatches[i][1]
+					zippedMatch := map[string]string{
+						"hostname": hostnameValue,
+						"cpus":     cpusValue,
+					}
+					zippedMatches = append(zippedMatches, zippedMatch)
+				}
+				for _, match := range zippedMatches {
+					fmt.Printf("Hostname: %s\n", match["hostname"])
+					fmt.Printf("CPUs: %s\n", match["cpus"])
+				}
+
+				ranges := strings.Split(zippedMatches[0]["cpus"], ",")
+				guaranteedPodCpus := make([]int, 0)
+
+				for _, valueStr := range ranges {
+					// Check if the value is a range
+					if strings.Contains(valueStr, "-") {
+						rangeValues := strings.Split(valueStr, "-")
+						minValue, _ := strconv.Atoi(rangeValues[0])
+						maxValue, _ := strconv.Atoi(rangeValues[1])
+						// Append the range of values to the slice
+						for i := minValue; i <= maxValue; i++ {
+							guaranteedPodCpus = append(guaranteedPodCpus, i)
+						}
+					} else {
+						value, _ := strconv.Atoi(valueStr)
+						guaranteedPodCpus = append(guaranteedPodCpus, value)
+					}
+				}
+
+				runcCpus := zippedMatches[1]["cpus"]
+
+				overlapFound := false
+				for _, guaranteedPodCpu := range guaranteedPodCpus {
+					for _, cpuRange := range strings.Split(runcCpus, ",") {
+						fmt.Print(cpuRange,"\t" ,guaranteedPodCpu,"\n")
+						if strings.Contains(cpuRange, "-") {
+							rangeValues := strings.Split(cpuRange, "-")
+							minCpu, _ := strconv.Atoi(rangeValues[0])
+							maxCpu, _ := strconv.Atoi(rangeValues[1])
+							if guaranteedPodCpu >= minCpu && guaranteedPodCpu <= maxCpu {
+								overlapFound = true
+								fmt.Println("Overlap found. Error. Not expected this")
+								fmt.Print("\n\n",minCpu,maxCpu,guaranteedPodCpu)
+								break
+							}
+						} else {
+							cpuNumber, _ := strconv.Atoi(cpuRange)
+							if cpuNumber == guaranteedPodCpu {
+								overlapFound = true
+								fmt.Print("Overlap Found.",cpuNumber,guaranteedPodCpu)
+								break
+							}
+						}
+					}
+				}
+				Expect(overlapFound).To(BeFalse())
 
 			})
-
 		})
-
 })
 
 func getGuaranteedPod(workerRTNode *corev1.Node) *corev1.Pod {
@@ -147,9 +205,11 @@ func getNonGuaranteedPod(workerRTNode *corev1.Node) *corev1.Pod {
 	testpod2 := pods.GetTestPod()
 	testpod2.Namespace = testutils.NamespaceTesting
 	testpod2.Spec.NodeSelector = map[string]string{testutils.LabelHostname: workerRTNode.Name}
+
 	profile, _ := profiles.GetByNodeLabels(testutils.NodeSelectorLabels)
 	runtimeClass := components.GetComponentName(profile.Name, components.ComponentNamePrefix)
 	testpod2.Spec.RuntimeClassName = &runtimeClass
+
 	err = testclient.Client.Create(context.TODO(), testpod2)
 	Expect(err).ToNot(HaveOccurred())
 	_,err = pods.WaitForCondition(client.ObjectKeyFromObject(testpod2), corev1.PodReady, corev1.ConditionTrue, 5*time.Minute)
@@ -159,7 +219,7 @@ func getNonGuaranteedPod(workerRTNode *corev1.Node) *corev1.Pod {
 
 func deletePod(pod *corev1.Pod) {
 	err := testclient.Client.Delete(context.TODO(), pod)
-		testlog.Error(err)
+	testlog.Error(err)
 
 	err = pods.WaitForDeletion(pod, pods.DefaultDeletionTimeout*time.Second)
 	if err != nil {
@@ -168,40 +228,37 @@ func deletePod(pod *corev1.Pod) {
 }
 
 func createWrapper(node *corev1.Node, filePath string) (bool, string) {
-	//if found , save the original script to restore it after the test , create it if not
+	// if found , save the original script to restore it after the test , create it if not
 	cmd := []string{"cat", filePath}
 	originalWrapper, err := nodes.ExecCommandOnNode(cmd, node)
+
 	wrapperFound := false
 	if err != nil {
-		fmt.Println("Wrapper Not Found")
 
-		cmd = []string{"/bin/bash","-c","echo '#!/bin/bash\nif [ -n \"$3\" ] && [ \"$3\" == \"create\" ] && [ -f \"$5/config.json\" ]; then\n        conf=\"$5/config.json\"\n        cat $conf >> /root/create\n\nfi\nexec /bin/runc \"$@\"' > /usr/local/bin/runc-wrapper.sh"}
-		createdWrapper, err := nodes.ExecCommandOnNode(cmd, node)
-		//Expect(err).ToNot(HaveOccurred(), "Failed to write to %s: %w", filePath, err)
-		//Returns timed out error everytime it writes to the file
+		cmd = []string{"/bin/bash","-c","echo '#!/bin/bash\nif [ -n \"$3\" ] && [ \"$3\" == \"create\" ] && [ -f \"$5/config.json\" ]; then\n        conf=\"$5/config.json\"\n        cat $conf >> /root/create\n\nfi\nexec /bin/runc \"$@\"' > /rootfs/usr/local/bin/runc-wrapper.sh"}
+		createdWrapper, _ := nodes.ExecCommandOnNode(cmd, node)
+		// Expect(err).ToNot(HaveOccurred(), "Failed to write to %s: %w", filePath, err)
+		// ECHO TIMEOUT ISSUE
 		fmt.Println("First time created wrapper")
 
-	    cmd = []string{"cat", "/usr/local/bin/runc-wrapper.sh"}
-		createdWrapper, err = nodes.ExecCommandOnNode(cmd, node)
-		Expect(err).ToNot(HaveOccurred(), "Failed to write to %s: %w", filePath, err)
-//		fmt.Print(createdWrapper, "\n\n")
+		cmd = []string{"chmod","a+x", "/rootfs/usr/local/bin/runc-wrapper.sh"}
+		_, err = nodes.ExecCommandOnNode(cmd, node)
+		//Expect(err).ToNot(HaveOccurred(), "Failed to make %s executable: %s", filePath, err)
+		// TIMEOUT ISSUE 
 
-		cmd = []string{"ls", "-l", "/usr/local/bin/"}
-		createdWrapper, err = nodes.ExecCommandOnNode(cmd, node)
-		fmt.Print(createdWrapper)
 		return wrapperFound, createdWrapper
 	} else {
-		fmt.Println("Wrapper Found")
+
 		wrapperFound = true
 		return wrapperFound, originalWrapper
 	}
 }
 
 func updateRuncRuntimePath(node *corev1.Node, filePath string, configPath string) string {
-	//update the runtime_path inside configPath
-	//newRuntimePath := "/usr/local/bin/runc-wrapper.sh"
 	cmd := []string{"cat", configPath}
 	originalFileContent, err := nodes.ExecCommandOnNode(cmd, node)
+	Expect(err).ToNot(HaveOccurred())
+
 	exp := `\[crio\.runtime\.runtimes\.runc\](\n.*)+runtime_path = "(.*)"(\n.*)+\[`
 	r1 := regexp.MustCompile(exp)
 	groups := r1.FindStringSubmatch(originalFileContent)
@@ -215,12 +272,9 @@ func updateRuncRuntimePath(node *corev1.Node, filePath string, configPath string
 
 	cmd = []string{"/bin/bash","-c",fmt.Sprintf("echo '%s' > /rootfs/etc/crio/crio.conf.d/99-runtimes.conf",updatedContent)}
 	_, err = nodes.ExecCommandOnNode(cmd, node)
-	//Expect(err).ToNot(HaveOccurred(), "Failed to update : %w", configPath, err)
-	fmt.Print(err)
-
-	cmd = []string{"cat",configPath}
-	out, err := nodes.ExecCommandOnNode(cmd, node)
-	fmt.Print("\n\n UPDATED FILE",out,"\n\n")
+	// Expect(err).ToNot(HaveOccurred(), "Failed to update : %w", configPath, err)
+	// ECHO TIMEOUT ISSUE
 
 	return originalFileContent
 }
+
