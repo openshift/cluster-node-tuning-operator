415e70e3017067b1522cd995b43a0a66f00a6eec
Niranjan M.R <mrniranjan@redhat.com>
2023-09-06 15:13:25 +0530

    Removed unused variables and minor review fixes
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>

--- test/e2e/performanceprofile/functests/10_performance_ppc/10_ppc_suite_test.go
+++ test/e2e/performanceprofile/functests/10_performance_ppc/10_ppc_suite_test.go
@@ -13,18 +13,16 @@ import (
 )
 
 const (
-	podmanBinary = "/usr/bin/podman"
+	DefaultPodmanBinaryPath = "/usr/bin/podman"
 )
 
-var DefaultNtoImage string
-var MustgatherDir string
 
 var _ = BeforeSuite(func() {
 	var err error
 	By("Check podman binary exists")
-	path, err := exec.LookPath(podmanBinary)
+	path, err := exec.LookPath(DefaultPodmanBinaryPath)
 	if err != nil {
-		Skip(fmt.Sprintf("%s doesn't exists", podmanBinary))
+		Skip(fmt.Sprintf("%s doesn't exists", DefaultPodmanBinaryPath))
 	}
 	testlog.Infof("Podman binary executed from path %s", path)
 	By("Checking Environment Variables")
@@ -32,11 +30,11 @@ var _ = BeforeSuite(func() {
 	if testutils.MustGatherDir == "" {
 		Skip("set env variable MUSTGATHER_DIR to ocp mustgather directory")
 	}
-	testlog.Infof("Mustgather Directory used: %s", MustgatherDir)
+	testlog.Infof("Mustgather Directory used: %s", testutils.MustGatherDir)
 
 })
 
-func Test9Ppc(t *testing.T) {
+func TestPPC(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "PPC Suite")
 }
