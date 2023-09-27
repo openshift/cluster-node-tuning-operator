9b24a46f5825042a6a1ce8d940d89734f567bca1
Talor Itzhak <titzhak@redhat.com>
2023-09-27 18:01:38 +0300

    rps: simplifiy `set-rps-mask.sh` script
    With the new udev rule, the full path passes to the systemd unit.
    Hence, we don't need jump through loops in order to figure out the
    physical device location.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- assets/performanceprofile/scripts/set-rps-mask.sh
+++ assets/performanceprofile/scripts/set-rps-mask.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env bash
 
-dev=$1
-[ -n "${dev}" ] || { echo "The device argument is missing" >&2 ; exit 1; }
+full_path=${1}
+[ -n "${full_path}" ] || { echo "The full device path argument is missing" >&2 ; exit 1; }
 
-mask=$2
+mask=${2}
 [ -n "${mask}" ] || { echo "The mask argument is missing" >&2 ; exit 1; }
 
-dev_dir="/sys/class/net/${dev}"
+# replace '-' with '/'
+de_escape_path="/sys${full_path//-//}"
 
-function find_dev_dir {
-  systemd_devs=$(systemctl list-units -t device | grep sys-subsystem-net-devices | cut -d' ' -f1)
+# get the path for the queues
+queues_path=${de_escape_path%/rx*}
 
-  for systemd_dev in ${systemd_devs}; do
-    dev_sysfs=$(systemctl show "${systemd_dev}" -p SysFSPath --value)
-
-    dev_orig_name="${dev_sysfs##*/}"
-    if [ "${dev_orig_name}" = "${dev}" ]; then
-      dev_name="${systemd_dev##*-}"
-      dev_name="${dev_name%%.device}"
-      if [ "${dev_name}" = "${dev}" ]; then # disregard the original device unit
-              continue
-      fi
-
-      echo "${dev} device was renamed to $dev_name"
-      dev_dir="/sys/class/net/${dev_name}"
-      break
-    fi
-  done
-}
-
-[ -d "${dev_dir}" ] || find_dev_dir                # the net device was renamed, find the new name
-[ -d "${dev_dir}" ] || { sleep 5; find_dev_dir; }  # search failed, wait a little and try again
-[ -d "${dev_dir}" ] || { echo "${dev_dir}" directory not found >&2 ; exit 0; } # the interface disappeared, not an error
-
-for i in "${dev_dir}"/queues/rx-*/rps_cpus; do
+# set rps affinity for all queues
+for i in "${queues_path}"/rx-*/rps_cpus; do
   echo "${mask}" > "${i}"
 done
