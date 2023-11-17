3bff64ea315e912f27bae79219414026140ab6cc
Francesco Romani <fromani@redhat.com>
2023-11-17 12:12:44 +0100

    perfprof: fix condition reason
    Per Condition type description,
    `Reason: one-word CamelCase reason for the condition's last transition`
    
    So let's fix existing conditions to make sure they are all indeed
    CamelCase one-words.
    
    It must be noted that even though the code was wrong, this
    is a breaking change.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/performanceprofile/controller/status.go
+++ pkg/performanceprofile/controller/status.go
@@ -25,7 +25,7 @@ const (
 	conditionReasonComponentsCreationFailed  = "ComponentCreationFailed"
 	conditionReasonMCPDegraded               = "MCPDegraded"
 	conditionFailedGettingMCPStatus          = "GettingMCPStatusFailed"
-	conditionKubeletFailed                   = "KubeletConfig failure"
+	conditionKubeletFailed                   = "KubeletConfigFailure"
 	conditionFailedGettingKubeletStatus      = "GettingKubeletStatusFailed"
 	conditionReasonTunedDegraded             = "TunedProfileDegraded"
 	conditionFailedGettingTunedProfileStatus = "GettingTunedStatusFailed"
