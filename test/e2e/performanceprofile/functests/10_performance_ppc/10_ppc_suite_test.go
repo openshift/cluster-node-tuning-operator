b41042d42d4ba5bb2e99960248cf1d6ae4935018
Niranjan M.R <niranjan@ashoo.in>
2023-11-30 16:51:11 +0530

    OCPBUGS-18649:  E2E: PPC Test cases (#708)
    * Add variables for mustgather and NTO image
    
    Adds to new environment variables
    NTO_IMAGE_REGISTRY and MUSGATHER_DIR for providing
    NTO image and mustgather directory respectively
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * gexec vendor updates
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * E2E: PPC test cases
    
    PPC Test cases executed through podman
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * Include E2E ppc to be executed as part of functional tests
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * Removed unused variables and minor review fixes
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * minor gofmt fixes
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * Remove hardcoded values of reserved and isolated
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * Remove unused struct and rename NtoImageRegistry as NtoImage
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * use cpuset to get length of cpus
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    * update the NTOImage variable
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    
    ---------
    
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    Co-authored-by: Niranjan M.R <mrniranjan@redhat.com>

---
+++ test/e2e/performanceprofile/functests/10_performance_ppc/10_ppc_suite_test.go
@@ -0,0 +1,39 @@
+package __performance_ppc
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+
+	. "github.com/onsi/gomega"
+	testutils "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils"
+	testlog "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/log"
+)
+
+const (
+	DefaultPodmanBinaryPath = "/usr/bin/podman"
+)
+
+var _ = BeforeSuite(func() {
+	var err error
+	By("Check podman binary exists")
+	path, err := exec.LookPath(DefaultPodmanBinaryPath)
+	if err != nil {
+		Skip(fmt.Sprintf("%s doesn't exists", DefaultPodmanBinaryPath))
+	}
+	testlog.Infof("Podman binary executed from path %s", path)
+	By("Checking Environment Variables")
+	testlog.Infof("NTO Image used: %s", testutils.NTOImage)
+	if testutils.MustGatherDir == "" {
+		Skip("set env variable MUSTGATHER_DIR to ocp mustgather directory")
+	}
+	testlog.Infof("Mustgather Directory used: %s", testutils.MustGatherDir)
+
+})
+
+func TestPPC(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "PPC Suite")
+}
