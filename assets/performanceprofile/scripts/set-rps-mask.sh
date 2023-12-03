9c43bc10d24526f6b1012828ae9b15e95ef6d710
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-12-03 13:53:21 +0100

    [release-4.14] OCPBUGS-21845:rps: trigger udev event per queue #832 (#832)
    * rps: trigger udev per queue
    
    Instead of trigger udev event per physical device creation,
    triggers it per queue creation so in case of queue resize
    (i.e. more queues are added) it applies the correct rps mask on the
    new created queues as well.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    
    * rps: simplifiy `set-rps-mask.sh` script
    
    With the new udev rule, the full path passes to the systemd unit.
    Hence, we don't need jump through loops in order to figure out the
    physical device location.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    
    * rps: do no escape instance name
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    
    * render-sync
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    
    ---------
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    Co-authored-by: Talor Itzhak <titzhak@redhat.com>

--- assets/performanceprofile/scripts/set-rps-mask.sh
+++ assets/performanceprofile/scripts/set-rps-mask.sh
@@ -1,38 +1,16 @@
 #!/usr/bin/env bash
 
-dev=$1
-[ -n "${dev}" ] || { echo "The device argument is missing" >&2 ; exit 1; }
+path=${1}
+[ -n "${path}" ] || { echo "The device path argument is missing" >&2 ; exit 1; }
 
-mask=$2
+mask=${2}
 [ -n "${mask}" ] || { echo "The mask argument is missing" >&2 ; exit 1; }
 
-dev_dir="/sys/class/net/${dev}"
+queue_path=${path%rx*}
 
-function find_dev_dir {
-  systemd_devs=$(systemctl list-units -t device | grep sys-subsystem-net-devices | cut -d' ' -f1)
+queue_num=${path#*queues/}
+# replace '/' with '-'
+queue_num="${queue_num/\//-}"
 
-  for systemd_dev in ${systemd_devs}; do
-    dev_sysfs=$(systemctl show "${systemd_dev}" -p SysFSPath --value)
-
-    dev_orig_name="${dev_sysfs##*/}"
-    if [ "${dev_orig_name}" = "${dev}" ]; then
-      dev_name="${systemd_dev##*-}"
-      dev_name="${dev_name%%.device}"
-      if [ "${dev_name}" = "${dev}" ]; then # disregard the original device unit
-              continue
-      fi
-
-      echo "${dev} device was renamed to $dev_name"
-      dev_dir="/sys/class/net/${dev_name}"
-      break
-    fi
-  done
-}
-
-[ -d "${dev_dir}" ] || find_dev_dir                # the net device was renamed, find the new name
-[ -d "${dev_dir}" ] || { sleep 5; find_dev_dir; }  # search failed, wait a little and try again
-[ -d "${dev_dir}" ] || { echo "${dev_dir}" directory not found >&2 ; exit 0; } # the interface disappeared, not an error
-
-for i in "${dev_dir}"/queues/rx-*/rps_cpus; do
-  echo "${mask}" > "${i}"
-done
+# set rps affinity for the queue
+echo "${mask}" > "/sys${queue_path}${queue_num}/rps_cpus"
