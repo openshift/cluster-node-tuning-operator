4f9b5acf91dc9d47994836c8ba79e61ebedb54d7
Talor Itzhak <titzhak@redhat.com>
2023-12-12 13:00:01 +0200

    rps:udev: escape systemd unit name
    The journal spammed by warnings about the fact that systemd unit name
    is not escaped.
    
    we use systemd-escape in order to escape the unit-name properly.
    `--path` is for telling systemd-escape that the input is a valid file path name.
    `--template` is telling systemd-escape to inserts the escaped strings in a unit name template.
    
    %c accepts the output of `PROGRAM`
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- assets/performanceprofile/scripts/set-rps-mask.sh
+++ assets/performanceprofile/scripts/set-rps-mask.sh
@@ -1,16 +1,37 @@
 #!/usr/bin/env bash
 
-path=${1}
-[ -n "${path}" ] || { echo "The device path argument is missing" >&2 ; exit 1; }
-
-mask=${2}
-[ -n "${mask}" ] || { echo "The mask argument is missing" >&2 ; exit 1; }
-
+function set_queue_rps_mask() {
 queue_path=${path%rx*}
 
 queue_num=${path#*queues/}
+
 # replace '/' with '-'
 queue_num="${queue_num/\//-}"
 
 # set rps affinity for the queue
-echo "${mask}" > "/sys${queue_path}${queue_num}/rps_cpus"
+echo "${mask}"  2> /dev/null > "/sys${queue_path}${queue_num}/rps_cpus"
+
+# the 'echo' command might failed if the device path which the queue belongs to has changes
+# this can happen in case of SRI-OV devices renaming
+exit 0
+}
+
+function set_net_dev_rps_mask() {
+  # in case of device we want to iterate through all queues
+   for i in /sys"${path}"/queues/rx-*; do
+     echo "${mask}" > "${i}/rps_cpus"
+  done
+ }
+
+ path=${1}
+ [ -n "${path}" ] || { echo "The device path argument is missing" >&2 ; exit 1; }
+
+ mask=${2}
+ [ -n "${mask}" ] || { echo "The mask argument is missing" >&2 ; exit 1; }
+
+ if [[ "${path}" =~ "queues" ]]; then
+   set_queue_rps_mask
+ else
+   set_net_dev_rps_mask
+ fi
+
