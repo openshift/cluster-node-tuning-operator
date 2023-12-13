0706a13feb84045d7a04c565d46233a6d60fad64
Jose Luis Ojosnegros Manchón <jojosneg@redhat.com>
2023-12-13 11:09:09 +0100

    e2e-test: Call SetLogger
    You must set a logger for controller-runtime, otherwise it complains.
    
    Signed-off-by: Jose Luis Ojosnegros Manchón <jojosneg@redhat.com>

--- test/e2e/basic/operator_test.go
+++ test/e2e/basic/operator_test.go
@@ -1,10 +1,15 @@
 package e2e
 
 import (
+	"log"
+	"os"
 	"testing"
 
+	"github.com/go-logr/stdr"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/openshift/cluster-node-tuning-operator/test/framework"
 )
@@ -14,6 +19,7 @@ var (
 )
 
 func TestNodeTuningOperator(t *testing.T) {
+	ctrllog.SetLogger(stdr.New(log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)))
 	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, "Node Tuning Operator e2e tests: basic")
 }
