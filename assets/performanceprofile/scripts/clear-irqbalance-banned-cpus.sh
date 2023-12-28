ea93668ced7a70cf35076fb4e18d04dc4532de0d
Talor Itzhak <titzhak@redhat.com>
2023-12-28 11:27:06 +0200

    bash: fix globbing and word splitting
    According to https://github.com/koalaman/shellcheck/wiki/SC2070
    `-n` doesn\'t work with unquoted arguments.
    
    In addition it's safer to always evaluate variables in quotes to avoid
    word splitting and globbing, so let\'s fix that as well.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- assets/performanceprofile/scripts/clear-irqbalance-banned-cpus.sh
+++ assets/performanceprofile/scripts/clear-irqbalance-banned-cpus.sh
@@ -9,17 +9,17 @@ IRQBALANCE_CONF="${1:-/etc/sysconfig/irqbalance}"
 CRIO_ORIG_BANNED_CPUS="${2:-/etc/sysconfig/orig_irq_banned_cpus}"
 NONE=0
 
-[ ! -f ${IRQBALANCE_CONF} ] && exit 0
+[ ! -f "${IRQBALANCE_CONF}" ] && exit 0
 
-${SED} -i '/^\s*IRQBALANCE_BANNED_CPUS\b/d' ${IRQBALANCE_CONF} || exit 0
+${SED} -i '/^\s*IRQBALANCE_BANNED_CPUS\b/d' "${IRQBALANCE_CONF}" || exit 0
 # CPU numbers which have their corresponding bits set to one in this mask
 # will not have any irq's assigned to them on rebalance.
 # so zero means all cpus are participating in load balancing.
-echo "IRQBALANCE_BANNED_CPUS=${NONE}" >> ${IRQBALANCE_CONF}
+echo "IRQBALANCE_BANNED_CPUS=${NONE}" >> "${IRQBALANCE_CONF}"
 
 # we now own this configuration. But CRI-O has code to restore the configuration,
 # and until it gains the option to disable this restore flow, we need to make
 # the configuration consistent such as the CRI-O restore will do nothing.
-if [ -n ${CRIO_ORIG_BANNED_CPUS} ] && [ -f ${CRIO_ORIG_BANNED_CPUS} ]; then
-	echo "${NONE}" > ${CRIO_ORIG_BANNED_CPUS}
+if [ -n "${CRIO_ORIG_BANNED_CPUS}" ] && [ -f "${CRIO_ORIG_BANNED_CPUS}" ]; then
+	echo "${NONE}" > "${CRIO_ORIG_BANNED_CPUS}"
 fi
