0f8ba44409a8e3446ebae80ded8b1ceb7504e8e5
Talor Itzhak <titzhak@redhat.com>
2024-02-13 01:01:11 +0200

    [release-4.15] [manual]  OCPBUGS-29376: e2e: cgroups: introduce cgroup package (#942)
    * e2e: change `ExecCommandOnPod` to accept container name
    
    In case no name provided, it takes the name from
    the first container in the pod.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    
    * e2e:utils: introduce cgroup package
    
    This package provides a ControllerGetter which is cgroup version agnostic.
    It provides a unified api for testing the mixedcpus feature for both v1 and v2.
    
    The package can be expand in the future for testing additional features
    that requires cgroups inspection.
    
    Also in the future this package should be runtime (crun/runc) agnostic.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    
    * cgroup: exec separatly for each controller file
    
    The exec command might return empty value.
    In order to have better control of what we get
    we should read the files in separate exec calls.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>
    
    ---------
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ test/e2e/performanceprofile/functests/utils/cgroup/v2/v2.go
@@ -0,0 +1,115 @@
+package v2
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/cgroup/controller"
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/cgroup/runtime"
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/log"
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/pods"
+)
+
+type ControllersManager struct {
+	client    client.Client
+	k8sClient *kubernetes.Clientset
+}
+
+func NewManager(c client.Client, k8sClient *kubernetes.Clientset) *ControllersManager {
+	return &ControllersManager{client: c, k8sClient: k8sClient}
+}
+
+func (cm *ControllersManager) CpuSet(ctx context.Context, pod *corev1.Pod, containerName, childName, runtimeType string) (*controller.CpuSet, error) {
+	cfg := &controller.CpuSet{}
+	dirPath := path.Join(controller.CgroupMountPoint, childName)
+	store := map[string]*string{
+		"cpuset.cpus":           &cfg.Cpus,
+		"cpuset.cpus.exclusive": &cfg.Exclusive,
+		"cpuset.cpus.effective": &cfg.Effective,
+		"cpuset.cpus.partition": &cfg.Partition,
+		"cpuset.mems":           &cfg.Mems,
+	}
+	err := cm.execAndStore(pod, containerName, dirPath, store)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve cgroup config for pod. pod=%q, container=%q; %w", client.ObjectKeyFromObject(pod).String(), containerName, err)
+	}
+	return cfg, nil
+}
+
+func (cm *ControllersManager) Cpu(ctx context.Context, pod *corev1.Pod, containerName, childName, runtimeType string) (*controller.Cpu, error) {
+	cfg := &controller.Cpu{}
+	dirPath := path.Join(controller.CgroupMountPoint, childName)
+	cmd := []string{
+		"/bin/cat",
+		dirPath + "/cpu.max",
+	}
+	b, err := pods.ExecCommandOnPod(cm.k8sClient, pod, containerName, cmd)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve cgroup config for pod. pod=%q, container=%q; %w", client.ObjectKeyFromObject(pod).String(), containerName, err)
+	}
+	output := strings.Split(string(b), "\r\n")
+	quotaAndPeriod := strings.Split(output[0], " ")
+	cfg.Quota = quotaAndPeriod[0]
+	cfg.Period = quotaAndPeriod[1]
+	return cfg, nil
+}
+
+func (cm *ControllersManager) Pod(ctx context.Context, pod *corev1.Pod, controllerConfig interface{}) error {
+	// TODO
+	return nil
+}
+
+func (cm *ControllersManager) Container(ctx context.Context, pod *corev1.Pod, containerName string, controllerConfig interface{}) error {
+	return cm.Child(ctx, pod, containerName, "", controllerConfig)
+}
+
+func (cm *ControllersManager) Child(ctx context.Context, pod *corev1.Pod, containerName, childName string, controllerConfig interface{}) error {
+	runtimeType, err := runtime.GetContainerRuntimeTypeFor(ctx, cm.client, pod)
+	if err != nil {
+		return err
+	}
+	switch cc := controllerConfig.(type) {
+	case *controller.CpuSet:
+		cfg, err := cm.CpuSet(ctx, pod, containerName, childName, runtimeType)
+		if err != nil {
+			return err
+		}
+		*cc = *cfg
+	case *controller.Cpu:
+		cfg, err := cm.Cpu(ctx, pod, containerName, childName, runtimeType)
+		if err != nil {
+			return err
+		}
+		*cc = *cfg
+	default:
+		return fmt.Errorf("failed to get the controller config type")
+	}
+	return nil
+}
+func (cm *ControllersManager) execAndStore(pod *corev1.Pod, containerName, dirPath string, store map[string]*string) error {
+	for k, v := range store {
+		fullPath := dirPath + "/" + k
+		cmd := []string{
+			"/bin/cat",
+			fullPath,
+		}
+		b, err := pods.ExecCommandOnPod(cm.k8sClient, pod, containerName, cmd)
+		if err != nil {
+			return err
+		}
+		if len(b) == 0 {
+			log.Warningf("empty value in cgroupv2 controller file; pod=%q,container=%q,file=%q", pod.Name, containerName, fullPath)
+			*v = ""
+			continue
+		}
+		output := strings.Trim(string(b), "\r\n")
+		*v = output
+	}
+	return nil
+}
