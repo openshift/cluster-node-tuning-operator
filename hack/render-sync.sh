19686cd969404eca0bf09f9c991a8e7bd7ea0ad2
Ronny Baturov <rbaturov@redhat.com>
2024-02-21 19:44:17 +0200

    OCPBUGS-23167: Add performance real time tuned template (#954)
    * Add performance real time tuned template
    
    Kernel parameters that are not supported on RT kernel systems are being applied, causing errors to be logged to the tuned daemon, resulting in a degraded profile state.
    Therefore, I added the openshift-node-performance-rt profile that would be included if an RT kernel is detected, thereby dropping the unsupported kernel parameters before they are applied.
    
    * Added a test that make sure the tuned profile is not degraded
    
    This could be great to have a generic test that make sure that after
    performance profile is applied the tuned profile is not degraded.
    This would prevent future issues like OCPBUGS-23167.
    
    * render-sync update
    
    Updated render-sync to include artifacts that are needed for the e2e tests.
    Committing the rendered items to this commit.

--- hack/render-sync.sh
+++ hack/render-sync.sh
@@ -41,3 +41,5 @@ function rendersync() {
 rendersync manual-cluster/performance base/performance default
 rendersync bootstrap-cluster/performance pinned-cluster/default bootstrap/no-mcp
 rendersync bootstrap-cluster/performance pinned-cluster/default bootstrap-cluster/extra-mcp bootstrap/extra-mcp
+rendersync --owner-ref none -- base/performance manual-cluster/performance no-ref 
+rendersync --owner-ref none -- base/performance manual-cluster/cpuFrequency default/cpuFrequency 
