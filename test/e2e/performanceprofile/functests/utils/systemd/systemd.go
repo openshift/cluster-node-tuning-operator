b6190f97d82c74b7dd4755816d0f4d892c7432a9
Niranjan M.R <mrniranjan@redhat.com>
2024-03-12 17:21:50 +0530

    Add utils function for systemd
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>

---
+++ test/e2e/performanceprofile/functests/utils/systemd/systemd.go
@@ -0,0 +1,21 @@
+package systemd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/nodes"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func Status(ctx context.Context, unitfile string, node *corev1.Node) (string, error) {
+	cmd := []string{"/bin/bash", "-c", fmt.Sprintf("chroot /rootfs systemctl status %s --lines=0 --no-pager", unitfile)}
+	out, err := nodes.ExecCommandOnMachineConfigDaemon(ctx, node, cmd)
+	return string(out), err
+}
+
+func ShowProperty(ctx context.Context, unitfile string, property string, node *corev1.Node) (string, error) {
+	cmd := []string{"/bin/bash", "-c", fmt.Sprintf("chroot /rootfs systemctl show -p %s %s --no-pager", property, unitfile)}
+	out, err := nodes.ExecCommandOnMachineConfigDaemon(ctx, node, cmd)
+	return string(out), err
+}
