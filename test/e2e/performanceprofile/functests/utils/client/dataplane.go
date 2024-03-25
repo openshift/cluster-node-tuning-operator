278dced04179efc2c3222ed3be899a36c35be178
Talor Itzhak <titzhak@redhat.com>
2024-03-25 15:57:59 +0200

    e2e:control and data planes clients
    On Hypershift we should be able to access both the management and hosted clusters.
    For that, we should now have two clients, each communicates with a different cluster.
    
    ControlPlaneClient - defines the API client to run CRUD operations on the control plane that will be used for testing.
    
    DataPlaneClient - defines the API client to run CRUD operations on the data plane that will be used for testing.
    
    On non-hypershift both client would point to the same the cluster.
    
    We also added Ginkgo assertions that prevent the user from using the wrong clients for the wrong objects.
    For example using the `DataPlaneClient` for querying the `PerformanceProfile` object
    is completley fine on non-hypershift cluster, but will fail badly on hypershift (since `PerformanceProfile`
    objects, are not presented on the hosted clusters.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ test/e2e/performanceprofile/functests/utils/client/dataplane.go
@@ -0,0 +1,70 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/hypershift"
+)
+
+type dataPlaneImpl struct {
+	client.Client
+}
+
+func (dpi *dataPlaneImpl) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
+	if hypershift.ObjIsEncapsulatedInConfigMap(obj) {
+		return fmt.Errorf("the provided object %s/%s might not be presented on hypershift cluster while using this client."+
+			"please use ControlPlaneClient client instead", obj.GetObjectKind(), obj.GetName())
+	}
+	return dpi.Client.Get(ctx, key, obj, opts...)
+}
+
+func (dpi *dataPlaneImpl) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
+	if hypershift.ObjIsEncapsulatedInConfigMap(list) {
+		return fmt.Errorf("the provided list of %s objects might not be presented on hypershift cluster while using this client."+
+			"please use ControlPlaneClient client instead", list.GetObjectKind())
+	}
+	return dpi.Client.List(ctx, list, opts...)
+}
+
+func (dpi *dataPlaneImpl) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+	if hypershift.ObjIsEncapsulatedInConfigMap(obj) {
+		return fmt.Errorf("the provided object %s/%s might not get created on hypershift cluster while using this client."+
+			"please use ControlPlaneClient client instead", obj.GetObjectKind(), obj.GetName())
+	}
+	return dpi.Client.Create(ctx, obj, opts...)
+}
+
+func (dpi *dataPlaneImpl) Delete(ctx context.Context, obj client.Object, opts ...client.DeleteOption) error {
+	if hypershift.ObjIsEncapsulatedInConfigMap(obj) {
+		return fmt.Errorf("the provided object %s/%s might not get deleted on hypershift cluster while using this client."+
+			"please use ControlPlaneClient client instead", obj.GetObjectKind(), obj.GetName())
+	}
+	return dpi.Client.Delete(ctx, obj, opts...)
+}
+
+func (dpi *dataPlaneImpl) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
+	if hypershift.ObjIsEncapsulatedInConfigMap(obj) {
+		return fmt.Errorf("the provided object %s/%s might not get updated on hypershift cluster while using this client."+
+			"please use ControlPlaneClient client instead", obj.GetObjectKind(), obj.GetName())
+	}
+	return dpi.Client.Update(ctx, obj, opts...)
+}
+
+func (dpi *dataPlaneImpl) Patch(ctx context.Context, obj client.Object, patch client.Patch, opts ...client.PatchOption) error {
+	if hypershift.ObjIsEncapsulatedInConfigMap(obj) {
+		return fmt.Errorf("the provided object %s/%s might not get patched on hypershift cluster while using this client."+
+			"please use ControlPlaneClient client instead", obj.GetObjectKind(), obj.GetName())
+	}
+	return dpi.Client.Patch(ctx, obj, patch, opts...)
+}
+
+func (dpi *dataPlaneImpl) DeleteAllOf(ctx context.Context, obj client.Object, opts ...client.DeleteAllOfOption) error {
+	if hypershift.ObjIsEncapsulatedInConfigMap(obj) {
+		return fmt.Errorf("the provided object %s/%s might not get deleted on hypershift cluster while using this client."+
+			"please use ControlPlaneClient client instead", obj.GetObjectKind(), obj.GetName())
+	}
+	return dpi.Client.DeleteAllOf(ctx, obj, opts...)
+}
