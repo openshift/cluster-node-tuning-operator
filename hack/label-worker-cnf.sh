8fe2c552ea8eed167694257b2a124a69029338b5
Ronny Baturov <rbaturov@redhat.com>
2024-06-09 17:42:26 +0300

    adjust hack/label-worker-cnf.sh script for hypershift
    We can now utilize this script to label a worker node in a hosted cluster.
    I tool some inspiration from the show-cluster-version.sh script.
    
    Signed-off-by: Ronny Baturov <rbaturov@redhat.com>

--- hack/label-worker-cnf.sh
+++ hack/label-worker-cnf.sh
@@ -6,8 +6,24 @@ set -e
 OC_TOOL="${OC_TOOL:-oc}"
 
 # Label 1 worker node
-echo "[INFO]: Labeling 1 worker node with worker-cnf"
-node=$(${OC_TOOL} get nodes --selector='node-role.kubernetes.io/worker' \
-    --selector='!node-role.kubernetes.io/master' -o name | head -1)
 
-${OC_TOOL} label --overwrite $node node-role.kubernetes.io/worker-cnf=""
+function label_worker_cnf() {
+    local kubeconfig="${1}"
+
+    echo "[INFO]: Labeling 1 worker node with worker-cnf"
+    if [[ -n "${kubeconfig}"  ]]; then
+        kubeconfig_arg="--kubeconfig=${kubeconfig}"
+    fi
+    node=$(${OC_TOOL} ${kubeconfig_arg} get nodes --selector='node-role.kubernetes.io/worker,!node-role.kubernetes.io/master' -o name | head -1)
+    echo ${OC_TOOL} label --overwrite $node node-role.kubernetes.io/worker-cnf=""
+    ${OC_TOOL} label ${kubeconfig_arg} --overwrite $node node-role.kubernetes.io/worker-cnf=""
+}
+
+if [[ ${CLUSTER_TYPE} == "hypershift" ]]; then
+    CLI_DIR="${CLI_DIR:-/usr/bin}"
+    OC_TOOL="${CLI_DIR}"/oc
+    echo $HYPERSHIFT_HOSTED_CLUSTER_KUBECONFIG
+    label_worker_cnf "${HYPERSHIFT_HOSTED_CLUSTER_KUBECONFIG}"
+else
+    label_worker_cnf
+fi
