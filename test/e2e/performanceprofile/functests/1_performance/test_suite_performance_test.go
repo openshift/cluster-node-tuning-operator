a2f06282d1990dced998f27bc7074a8841abba30
Ronny Baturov <rbaturov@redhat.com>
2024-06-10 10:16:37 +0300

    e2e: refactor client usage to controlplane and dataplane
    Refactor the functions and client calls in the 1_performance suite according to resource location. For instance, resources like the performance profile that reside in the management cluster will need to utilize the control plane client, whereas resources that reside on the hosted cluster will utilize the dataplane client.
    Reminder: On OCP, both control plane and dataplane clients will be based on the same KUBECONFIG, so they will have the same effect.
    
    Signed-off-by: Ronny Baturov <rbaturov@redhat.com>

--- test/e2e/performanceprofile/functests/1_performance/test_suite_performance_test.go
+++ test/e2e/performanceprofile/functests/1_performance/test_suite_performance_test.go
@@ -42,7 +42,7 @@ func init() {
 var _ = BeforeSuite(func() {
 	Expect(testclient.ClientsEnabled).To(BeTrue(), "package client not enabled")
 	// create test namespace
-	err := testclient.Client.Create(context.TODO(), namespaces.TestingNamespace)
+	err := testclient.DataPlaneClient.Create(context.TODO(), namespaces.TestingNamespace)
 	if errors.IsAlreadyExists(err) {
 		testlog.Warning("test namespace already exists, that is unexpected")
 		return
@@ -51,7 +51,7 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
-	err := testclient.Client.Delete(context.TODO(), namespaces.TestingNamespace)
+	err := testclient.DataPlaneClient.Delete(context.TODO(), namespaces.TestingNamespace)
 	Expect(err).ToNot(HaveOccurred())
 	err = namespaces.WaitForDeletion(testutils.NamespaceTesting, 5*time.Minute)
 })
