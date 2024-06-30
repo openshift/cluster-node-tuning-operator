dc57114c3f6160b7799ec601aed9ad20e9c11dc3
Ronny Baturov <rbaturov@redhat.com>
2024-06-30 15:28:34 +0300

    Changed node inspector to have lazy initialization
    To streamline the deployment of the node inspector, we have implemented a change where the ExecCommand will automatically deploy the node inspector if it is not already running. This approach offers several benefits:
    
    * On-Demand Deployment: The node inspector is deployed only when needed.
    * Users no longer need to explicitly deploy the inspector using the 0_config suite, allowing them to run specific specs or suites without verifying the node inspector's deployment status.
    
    Moreover, this commit introduces some API simplification as we abstracted this process from the user.
    * Lowercasing functions that are not intended to be called from outside the package.
    * It is obvious that we would always use the Dataplane client, hence removed it from all function signatures.
    * Introducing global variables for the namespace and node inspector, as these values remain constant.
    
    Signed-off-by: Ronny Baturov <rbaturov@redhat.com>

--- test/e2e/performanceprofile/functests/Z_deconfig/deconfig.go
+++ test/e2e/performanceprofile/functests/Z_deconfig/deconfig.go
@@ -7,7 +7,6 @@ import (
 	. "github.com/onsi/gomega"
 
 	testutils "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils"
-	testclient "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/client"
 	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/namespaces"
 	nodeInspector "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/node_inspector"
 )
@@ -16,7 +15,7 @@ import (
 
 var _ = Describe("Deconfig", func() {
 	It("should delete the node inspector and its namespace", func() {
-		err := nodeInspector.Delete(testclient.DataPlaneClient, testutils.NodeInspectorNamespace, testutils.NodeInspectorName)
+		err := nodeInspector.Delete()
 		Expect(err).ToNot(HaveOccurred())
 		err = namespaces.WaitForDeletion(testutils.NodeInspectorNamespace, 5*time.Minute)
 		Expect(err).ToNot(HaveOccurred())
