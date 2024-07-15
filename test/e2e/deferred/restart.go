73855b9bafd7b95a217db7b415ae20e84b1b691f
Francesco Romani <fromani@redhat.com>
2024-07-15 11:00:27 +0200

    e2e: deferred: check restore after restart
    Add test to verify that node state is correctly restored
    after a deferred update is applied (requires a restart)
    and then removed
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/deferred/restart.go
+++ test/e2e/deferred/restart.go
@@ -27,7 +27,9 @@ var _ = ginkgo.Describe("[deferred][restart] Profile deferred", func() {
 			targetNode    *corev1.Node
 
 			dirPath            string
+			tunedPathSHMMNI    string
 			tunedPathVMLatency string
+			tunedObjSHMMNI     *tunedv1.Tuned
 			tunedObjVMLatency  *tunedv1.Tuned
 		)
 
@@ -44,6 +46,10 @@ var _ = ginkgo.Describe("[deferred][restart] Profile deferred", func() {
 
 			dirPath, err = getCurrentDirPath()
 			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+			tunedPathSHMMNI = filepath.Join(dirPath, tunedSHMMNI)
+			tunedObjSHMMNI, err = loadTuned(tunedPathSHMMNI)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 			tunedPathVMLatency = filepath.Join(dirPath, tunedVMLatency)
 			tunedObjVMLatency, err = loadTuned(tunedPathVMLatency)
@@ -271,6 +277,142 @@ var _ = ginkgo.Describe("[deferred][restart] Profile deferred", func() {
 						if out != verif.output {
 							return fmt.Errorf("got: %s; expected: %s", out, verif.output)
 						}
+					}
+					return nil
+				}).WithPolling(10 * time.Second).WithTimeout(1 * time.Minute).Should(gomega.Succeed())
+			})
+
+			ginkgo.It("should be reverted once applied and the node state should be restored", func(ctx context.Context) {
+				tunedImmediate := tunedObjSHMMNI
+
+				verifications := extractVerifications(tunedImmediate)
+				gomega.Expect(len(verifications)).To(gomega.Equal(1), "unexpected verification count, check annotations")
+
+				ginkgo.By(fmt.Sprintf("getting the tuned pod running on %q", targetNode.Name))
+				targetTunedPod, err := util.GetTunedForNode(cs, targetNode)
+				gomega.Expect(err).NotTo(gomega.HaveOccurred())
+				ginkgo.By(fmt.Sprintf("got the tuned pod running on %q: %s/%s %s", targetNode.Name, targetTunedPod.Namespace, targetTunedPod.Name, targetTunedPod.UID))
+
+				verifCtx := mustExtractVerificationOutputAndCommand(targetNode, tunedImmediate)
+
+				tunedMutated := setDeferred(tunedImmediate.DeepCopy())
+				ginkgo.By(fmt.Sprintf("creating tuned object %s deferred=%v", tunedMutated.Name, ntoutil.HasDeferredUpdateAnnotation(tunedMutated.Annotations)))
+
+				_, err = cs.Tuneds(ntoconfig.WatchNamespace()).Create(ctx, tunedMutated, metav1.CreateOptions{})
+				gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+				createdTuneds = prepend(createdTuneds, tunedPathSHMMNI) // we need the path, not the name
+
+				gomega.Expect(tunedMutated.Spec.Recommend).ToNot(gomega.BeEmpty(), "tuned %q has empty recommendations", tunedMutated.Name)
+				gomega.Expect(tunedMutated.Spec.Recommend[0].Profile).ToNot(gomega.BeNil(), "tuned %q has empty recommended tuned profile", tunedMutated.Name)
+				expectedProfile := *tunedMutated.Spec.Recommend[0].Profile
+				ginkgo.By(fmt.Sprintf("expecting Tuned Profile %q to be picked up", expectedProfile))
+
+				gomega.Eventually(func() error {
+					curProf, err := cs.Profiles(ntoconfig.WatchNamespace()).Get(ctx, targetNode.Name, metav1.GetOptions{})
+					if err != nil {
+						return err
+					}
+					ginkgo.By(fmt.Sprintf("checking profile for target node %q matches expectations about %q", curProf.Name, expectedProfile))
+
+					if len(curProf.Status.Conditions) == 0 {
+						return fmt.Errorf("missing status conditions")
+					}
+					cond := findCondition(curProf.Status.Conditions, tunedv1.TunedProfileApplied)
+					if cond == nil {
+						return fmt.Errorf("missing status applied condition")
+					}
+					err = checkAppliedConditionDeferred(cond, expectedProfile)
+					if err != nil {
+						util.Logf("profile for target node %q does not match expectations about %q: %v", curProf.Name, expectedProfile, err)
+					}
+					return err
+				}).WithPolling(10 * time.Second).WithTimeout(1 * time.Minute).Should(gomega.Succeed())
+
+				ginkgo.By(fmt.Sprintf("getting the machine config daemon pod running on %q", targetNode.Name))
+				targetMCDPod, err := util.GetMachineConfigDaemonForNode(cs, targetNode)
+				gomega.Expect(err).NotTo(gomega.HaveOccurred())
+				ginkgo.By(fmt.Sprintf("got the machine config daemon pod running on %q: %s/%s %s", targetNode.Name, targetMCDPod.Namespace, targetMCDPod.Name, targetMCDPod.UID))
+
+				ginkgo.By(fmt.Sprintf("restarting the worker node on which the tuned was running on %q", targetNode.Name))
+				_, err = util.ExecCmdInPodNamespace(targetMCDPod.Namespace, targetMCDPod.Name, "chroot", "/rootfs", "systemctl", "reboot")
+				gomega.Expect(err).NotTo(gomega.HaveOccurred())
+				// Very generous timeout. On baremetal a reboot can take a long time
+				ready.WaitNodeOrFail(cs, "post-reboot", targetNode.Name, 20*time.Minute, 5*time.Second)
+
+				gomega.Eventually(func() error {
+					curProf, err := cs.Profiles(ntoconfig.WatchNamespace()).Get(ctx, targetNode.Name, metav1.GetOptions{})
+					if err != nil {
+						return err
+					}
+					ginkgo.By(fmt.Sprintf("checking profile for target node %q matches expectations about %q", curProf.Name, expectedProfile))
+
+					if len(curProf.Status.Conditions) == 0 {
+						return fmt.Errorf("missing status conditions")
+					}
+					cond := findCondition(curProf.Status.Conditions, tunedv1.TunedProfileApplied)
+					if cond == nil {
+						return fmt.Errorf("missing status applied condition")
+					}
+					err = checkAppliedConditionOK(cond)
+					if err != nil {
+						util.Logf("profile for target node %q does not match expectations about %q: %v", curProf.Name, expectedProfile, err)
+					}
+					return err
+				}).WithPolling(10 * time.Second).WithTimeout(1 * time.Minute).Should(gomega.Succeed())
+
+				ginkgo.By(fmt.Sprintf("getting again the tuned pod running on %q", targetNode.Name))
+				targetTunedPod, err = util.GetTunedForNode(cs, targetNode)
+				gomega.Expect(err).NotTo(gomega.HaveOccurred())
+				ginkgo.By(fmt.Sprintf("got again the tuned pod running on %q: %s/%s %s", targetNode.Name, targetTunedPod.Namespace, targetTunedPod.Name, targetTunedPod.UID))
+
+				gomega.Consistently(func() error {
+					ginkgo.By(fmt.Sprintf("checking real node conditions for profile %q", targetNode.Name))
+					for _, verif := range verifications {
+						out, err := util.ExecCmdInPod(targetTunedPod, verif.command...)
+						if err != nil {
+							return err
+						}
+						out = strings.TrimSpace(out)
+						if out != verif.output {
+							return fmt.Errorf("got: %s; expected: %s", out, verif.output)
+						}
+					}
+					return nil
+				}).WithPolling(10 * time.Second).WithTimeout(1 * time.Minute).Should(gomega.Succeed())
+
+				ginkgo.By(fmt.Sprintf("cluster changes rollback: %q", tunedPathSHMMNI))
+				_, _, err = util.ExecAndLogCommand("oc", "delete", "-n", ntoconfig.WatchNamespace(), "-f", tunedPathSHMMNI)
+				gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+				_, createdTuneds, _ = popleft(createdTuneds)
+				gomega.Eventually(func() error {
+					curProf, err := cs.Profiles(ntoconfig.WatchNamespace()).Get(ctx, targetNode.Name, metav1.GetOptions{})
+					if err != nil {
+						return err
+					}
+					ginkgo.By(fmt.Sprintf("checking profile for target node %q matches expectations about %q", curProf.Name, expectedProfile))
+
+					if len(curProf.Status.Conditions) == 0 {
+						return fmt.Errorf("missing status conditions")
+					}
+					cond := findCondition(curProf.Status.Conditions, tunedv1.TunedProfileApplied)
+					if cond == nil {
+						return fmt.Errorf("missing status applied condition")
+					}
+					err = checkAppliedConditionOK(cond)
+					if err != nil {
+						util.Logf("profile for target node %q does not match expectations about %q: %v", curProf.Name, expectedProfile, err)
+					}
+
+					ginkgo.By("checking real node conditions are restored pristine")
+					out, err := util.ExecCmdInPod(targetTunedPod, verifCtx.args...)
+					if err != nil {
+						return err
+					}
+					out = strings.TrimSpace(out)
+					if out != verifCtx.output {
+						return fmt.Errorf("got: %s; expected: %s", out, verifCtx.output)
 					}
 					return nil
 				}).WithPolling(10 * time.Second).WithTimeout(1 * time.Minute).Should(gomega.Succeed())
