a6c39f0ede612dfe9ba78b9712ab5a2ae7b007ac
Shereen Haj <shajmakh@redhat.com>
2024-11-18 11:00:53 +0200

    OCPBUGS-44372: PPC: skip comparing ProcessorCore.Index between NUMA cores  (#1213)
    * PPC: skip comparing ProcessorCore.Index between NUMA cores
    
    ProcessorCore.Index indicates the zero-based index of the core in the
    Cores slice. While core might be shown in a different order, they can still
    be equivalent. See: jaypipes/ghw#346.
    
    Adjust the equality check to skip this field to fix this:
    
    ```
      Error: targeted nodes differ: nodes host1.development.lab and host2.development.lab have different topology: the CPU cores differ: processor core #20 (2 threads), logical processors [2 66] vs processor core #20 (2 threads), logical processors [2 66]
    ```
    
    And add a unit test to cover this scenario.
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>
    
    * PPC: unit: rename test variables to avoid misusing
    
    Rename test variables and add clarifying comments to avoid misusing them
    while writing tests.
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>
    
    ---------
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>
    (cherry picked from commit ad4a69f4802078f63ca72bcd01815fae4f82c7bd)

--- pkg/performanceprofile/profilecreator/profilecreator.go
+++ pkg/performanceprofile/profilecreator/profilecreator.go
@@ -708,6 +708,8 @@ func EnsureNodesHaveTheSameHardware(nodeHandlers []*GHWHandler) error {
 }
 
 func ensureSameTopology(topology1, topology2 *topology.Info) error {
+	// the assumption here is that both topologies are deep sorted (e.g. slices of numa nodes, cores, processors ..);
+	// see handle.SortedTopology()
 	if topology1.Architecture != topology2.Architecture {
 		return fmt.Errorf("the architecture is different: %v vs %v", topology1.Architecture, topology2.Architecture)
 	}
@@ -725,13 +727,21 @@ func ensureSameTopology(topology1, topology2 *topology.Info) error {
 		cores1 := node1.Cores
 		cores2 := node2.Cores
 		if len(cores1) != len(cores2) {
-			return fmt.Errorf("the number of CPU cores in NUMA node %d differ: %v vs %v",
+			return fmt.Errorf("the number of CPU cores in NUMA node %d differs: %v vs %v",
 				node1.ID, len(topology1.Nodes), len(topology2.Nodes))
 		}
 
 		for j, core1 := range cores1 {
-			if !reflect.DeepEqual(core1, cores2[j]) {
-				return fmt.Errorf("the CPU corres differ: %v vs %v", core1, cores2[j])
+			// skip comparing index because it's fine if they differ; see https://github.com/jaypipes/ghw/issues/345#issuecomment-1620274077
+			// ghw.ProcessorCore.Index is completely removed starting v0.11.0
+			if core1.ID != cores2[j].ID {
+				return fmt.Errorf("the CPU core ids in NUMA node %d differ: %d vs %d", node1.ID, core1.ID, cores2[j].ID)
+			}
+			if core1.NumThreads != cores2[j].NumThreads {
+				return fmt.Errorf("number of threads for CPU %d in NUMA node %d differs: %d vs %d", core1.ID, node1.ID, core1.NumThreads, cores2[j].NumThreads)
+			}
+			if !reflect.DeepEqual(core1.LogicalProcessors, cores2[j].LogicalProcessors) {
+				return fmt.Errorf("logical processors for CPU %d in NUMA node %d differ: %d vs %d", core1.ID, node1.ID, core1.NumThreads, cores2[j].NumThreads)
 			}
 		}
 	}
