a9b1cdf11d112f714d023137183ae21ee3fef9a4
Roy Shemtov <rshemtov@redhat.com>
2025-03-30 20:20:41 +0300

    e2e: arm: Add kernel page size e2e testing suite (#1307)
    This PR introduces the following changes:
    
    * A new 14_arm directory, which contains the e2e test suite for validating support of the 64k kernelPageSize under PerformanceProfile for the aarch64 family of CPUs.
    
    * A new test suite to validate, at the node level, the kernelPageSize values of 4k and 64k on aarch64. This is done by applying changes to the spec field and inspecting the node to verify that the correct values are reflected on the associated node.
    
    The tests in the suite have been tested in an ARM cluster
    
    Signed-off-by: Roy Shemtov <rshemtov@redhat.com>

---
+++ test/e2e/performanceprofile/functests/14_arm/test_suite_arm_test.go
@@ -0,0 +1,28 @@
+package __arm
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/go-logr/stdr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/onsi/ginkgo/v2/reporters"
+	qe_reporters "kubevirt.io/qe-tools/pkg/ginkgo-reporters"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func TestARM(t *testing.T) {
+	ctrllog.SetLogger(stdr.New(log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)))
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Kernel page size suite")
+}
+
+var _ = ReportAfterSuite("kernel page size suite", func(r Report) {
+	if qe_reporters.Polarion.Run {
+		reporters.ReportViaDeprecatedReporter(&qe_reporters.Polarion, r)
+	}
+})
