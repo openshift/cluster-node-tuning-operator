fb5e5f9ed7874d3e59789b1b821ca6d400b9bb68
Sanika Patil <sanipati@redhat.com>
2026-09-22 23:50:38 +0530

    OSDOCS-22738: Fix hexadecimal typos in PerformanceProfile device comments Correct hexmadecimal/Hexmadecimal in PerformanceProfile .net.devices API comments so generated Node APIs docs render correctly.

--- pkg/apis/performanceprofile/v2/performanceprofile_types.go
+++ pkg/apis/performanceprofile/v2/performanceprofile_types.go
@@ -222,10 +222,10 @@ type Device struct {
 	// Network device name to be matched. It uses a syntax of shell-style wildcards which are either positive or negative.
 	// +optional
 	InterfaceName *string `json:"interfaceName,omitempty"`
-	// Network device vendor ID represnted as a 16 bit Hexmadecimal number.
+	// Network device vendor ID represnted as a 16 bit hexadecimal number.
 	// +optional
 	VendorID *string `json:"vendorID,omitempty"`
-	// Network device ID (model) represnted as a 16 bit hexmadecimal number.
+	// Network device ID (model) represnted as a 16 bit hexadecimal number.
 	// +optional
 	DeviceID *string `json:"deviceID,omitempty"`
 }
